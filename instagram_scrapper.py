606f87ff82aeed64d80cb1cb13a9a82786ab5bdf
AhmedSelim <31182803+Eng-AhmedSelim@users.noreply.github.com>
2021-09-11 21:37:00 +0300

    Update instagram_scrapper.py

--- instagram_scrapper.py
+++ instagram_scrapper.py
@@ -6,20 +6,21 @@ from selenium.webdriver.common.by import By
 username = ''
 password = ''
 
-driver = webdriver.Firefox()
+# Search selenium ChromeDriver or Firefox / and download deiver in folder project
+driver = webdriver.Firefox()  #Open Brawser Firefox
 driver.get('https://www.instagram.com/accounts/login/')
 sleep(6)
 driver.find_element_by_name('username').send_keys(username)
 driver.find_element_by_name('password').send_keys(password)
 sleep(5)
-driver.find_element_by_name('password').send_keys(Keys.RETURN)
+driver.find_element_by_name('password').send_keys(Keys.RETURN) # click inter
 sleep(5)
-driver.get('https://www.instagram.com/'+'therock')
+driver.get('https://www.instagram.com/'+'therock') # move to page rock
 sleep(5)
-follw_btn = driver.find_element(By.XPATH, '//button[text()="Follow"]')
+follw_btn = driver.find_element(By.XPATH, '//button[text()="Follow"]') # find but
 sleep(5)
-follw_btn.click()
+follw_btn.click() # click Button
 sleep(7)
-driver.refresh()
+driver.refresh() # refresh page
 
 
